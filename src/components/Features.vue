fe24e62eda08e04f261009bdb3d1cc508b85112f
Paulpov <80074076+Paulpov@users.noreply.github.com>
2023-08-15 12:13:16 -0300

    BButton Links

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="bg-bookmark-blue py-20 mt-20 pb-60 lg:mt-60">
     <!-- Heading-->
-    <div class="sm:w-3/4 lg:w-5/12 mx-auto px-2">
+    <div id="servicos" class="sm:w-3/4 lg:w-5/12 mx-auto px-2">
       <h1 class="text-4xl text-center text-bookmark-white">Nossos Serviços</h1>
       <p class="text-center text-bookmark-grey mt-4">
         Oferecemos soluções personalizadas de transporte, incluindo carga
@@ -52,12 +52,12 @@
     </div>
 
     <!-- Feature #2-->
-    <div class="relative mt-20 lg:mt-52">
+    <div  class="relative mt-20 lg:mt-52">
       <div
         class="container flex flex-col lg:flex-row-reverse items-center justify-center gap-x-24"
       >
         <!-- Feature #1-->
-        <div class="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
+        <div id="endereco" class="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
           <iframe
             class="rounded-lg flex flex-1 justify-center z-10 mb-10 lg:mb-0"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3662.9972343470295!2d-51.16579132385243!3d-23.35211475393978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94eb4320b03c0001%3A0xcb99e40d3214f44e!2sPopov%20Transportes!5e0!3m2!1spt-BR!2sbr!4v1691787008195!5m2!1spt-BR!2sbr"
@@ -121,7 +121,7 @@
           </div>
         </div>
         <!-- Content-->
-        <div class="flex flex-1 flex-col items-center lg:items-start">
+        <div id="comprometimento" class="flex flex-1 flex-col items-center lg:items-start">
           <h1 class="text-3xl text-bookmark-white">Comprometimento</h1>
           <p
             class="text-bookmark-grey my-4 text-center lg:text-left sm:w-3/4 lg:w-full"
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,10 +3,10 @@
     
     <div class="py-1"><img src="../assets/images/white-logo.png" width="120" alt="podcast logo"></div>
       <ul class="hidden sm:flex flex-1 justify-end items-center gap-12 text-bookmark-blue uppercase text-xs">
-        <li class="cursor-pointer text-white">Nossos Serviços</li>
-        <li class="cursor-pointer text-white">Endereço</li>
-        <li class="cursor-pointer text-white">Comprometimento</li>
-        <button type="button" class="bg-bookmark-red text-white rounded-md px-7 py-3 uppercase">Fale Conosco</button>
+        <li class="cursor-pointer text-white"> <a href="#servicos">Nossos Serviços</a></li>
+        <li class="cursor-pointer text-white"><a href="#endereco">Endereço</a></li>
+        <li class="cursor-pointer text-white"><a href="#comprometimento">Comprometimento</a></li>
+        <button type="button" class="bg-bookmark-red text-white rounded-md px-7 py-3 uppercase"><a href="#contato">Fale Conosco</a></button>
       </ul>
       <div class="flex sm:hidden flex-1 justify-end"><i class="text-xl fas fa-bars text-white cursor-pointer"></i></div>
   </nav>
