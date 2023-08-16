dcec8a5bb1aa7c662c6a3f52f5c7932947570d47
Paulpov <80074076+Paulpov@users.noreply.github.com>
2023-08-16 11:34:04 -0300

    lil updage

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,31 +1,73 @@
 <template>
-    <section class="relative">
-        <div class="container flex flex-col-reverse lg:flex-row items-center pag-12 mt-14 lg:mt-28">
-            <div class="flex flex-1 flex-col items-center lg:items-start">
-                <h2 class="text-white text-3xl md:text-4 leg:text-5xl text-center lg:text-left mb-6">Popov Transportes.</h2>
-                <p class="text-bookmark-grey text-lg text-center lg:text-left mb-6">com mais de 10 anos no mercado, destaca-se no setor de transporte pela qualidade, eficiência e abordagem humanizada no atendimento a clientes e colaboradores. A empresa promove um ambiente de trabalho saudável, valorizando o desenvolvimento pessoal dos colaboradores. A tecnologia é integrada aos processos para otimização contínua, reforçando o compromisso com excelência e aprimoramento constante no serviço de transporte.</p>
-                <div class="flex justify-center felx-wrap gap-6">
-                    <button type="button"
-                        class="btn btn-red hover:bg-bookmark-white hover:text-black">Saiba mais</button>
-                </div>
-            </div>
-            <div class="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0 z-10">
-                <img src="../assets/images/brand-logo.png" alt="micro illustration"
-                    class="w-3/4 h-3/4 sm:w-3/4 sm:h-3/4 md:w-3/5 md:h-3/5 rounded-xl">
-            </div>
-            <div
-                class="hidden md:block overflow-hidden bg-bookmark-blue rounded-l-full absolute h-80 w-2/4 top-32 right-0 lg:-bottom-28 lg:-right-30">
-            </div>
+  <section class="relative ">
+    
+    <div
+      class="container flex flex-col-reverse lg:flex-row items-center pag-12 mt-14 lg:mt-28 "
+    >
 
+
+        
+
+
+      <div class="flex flex-1 flex-col items-center lg:items-start ">
+
+        
             
 
+
+        <h2
+          class="text-white text-3xl md:text-4 leg:text-5xl text-center lg:text-left mb-6"
+        >
+          Popov Transportes.
+        </h2>
+        <p class="text-bookmark-grey text-lg text-center lg:text-left mb-6">
+          com mais de 10 anos no mercado, destaca-se no setor de transporte pela
+          qualidade, eficiência e abordagem humanizada no atendimento a clientes
+          e colaboradores. A empresa promove um ambiente de trabalho saudável,
+          valorizando o desenvolvimento pessoal dos colaboradores. A tecnologia
+          é integrada aos processos para otimização contínua, reforçando o
+          compromisso com excelência e aprimoramento constante no serviço de
+          transporte.
+        </p>
+        <div class="flex justify-center felx-wrap gap-6">
+          <div
+            class="flex flex-col sm:flex-row gap-6 mt-8 justify-center"
+          ></div>
         </div>
-    </section>
+      </div>
+      <div class="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0 z-10">
+        <img
+          src="../assets/images/brand-logo.png"
+          alt="micro illustration"
+          class="w-3/4 h-3/4 sm:w-3/4 sm:h-3/4 md:w-3/5 md:h-3/5 rounded-xl"
+        />
+      </div>
+      <div
+        class="hidden md:block overflow-hidden bg-bookmark-blue rounded-l-full absolute h-80 w-2/4 top-32 right-0 lg:-bottom-28 lg:-right-30"
+      ></div>
+    </div>
+  </section>
 </template>
 
-<script>
-export default {
+<script setup>
+import { ref } from "vue";
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/vue";
 
+const isOpen = ref(false);
+
+function closeModal() {
+  isOpen.value = false;
 }
+function openModal() {
+  isOpen.value = true;
+}
+
+
 </script>
 
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -6,14 +6,140 @@
         <li class="cursor-pointer text-white"> <a href="#servicos">Nossos Serviços</a></li>
         <li class="cursor-pointer text-white"><a href="#endereco">Endereço</a></li>
         <li class="cursor-pointer text-white"><a href="#comprometimento">Comprometimento</a></li>
-        <button type="button" class="bg-bookmark-red text-white rounded-md px-7 py-3 uppercase"><a href="#contato">Fale Conosco</a></button>
+        <div class="inset-0 flex items-center justify-center">
+              <button
+                type="button"
+                @click="openModal"
+                class="text-white flex-1 px-2 py-3 rounded-md btn bg-bookmark-red hover:bg-bookmark-white hover:text-black"
+              >
+                FALE CONOSCO
+              </button>
+            </div>
+            <TransitionRoot appear :show="isOpen" as="template">
+              <Dialog as="div" @close="closeModal" class="relative z-10">
+                <TransitionChild
+                  as="template"
+                  enter="duration-300 ease-out"
+                  enter-from="opacity-0"
+                  enter-to="opacity-100"
+                  leave="duration-200 ease-in"
+                  leave-from="opacity-100"
+                  leave-to="opacity-0"
+                >
+                  <div class="fixed inset-0 bg-black bg-opacity-25" />
+                </TransitionChild>
+
+                <div class="fixed inset-0 overflow-y-auto">
+                  <div
+                    class="flex min-h-full items-center justify-center p-4 text-center"
+                  >
+                    <TransitionChild
+                      as="template"
+                      enter="duration-300 ease-out"
+                      enter-from="opacity-0 scale-95"
+                      enter-to="opacity-100 scale-100"
+                      leave="duration-200 ease-in"
+                      leave-from="opacity-100 scale-100"
+                      leave-to="opacity-0 scale-95"
+                    >
+                      <DialogPanel
+                        class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+                      >
+                        <DialogTitle as="h3" class="text-2xl text-center">
+                          Nossos Contatos
+                        </DialogTitle>
+                        <div class="mt-4">
+                          <p class="text-sm text-gray-500">
+                            Abaixo estão nossos numeros e setores para melhor te
+                            atender!
+                          </p>
+
+                          <hr class="mt-4" />
+
+                          <div class="justify-center">
+                            <div
+                              class="flex flex-col sm:flex-row gap-6 mt-8 inset-0 flex items-center"
+                            >
+                              <p class="text-sm text-gray-500">
+                                Administração - (43) 99183-1471
+                              </p>
+                              <button
+                                type="button"
+                                class="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
+                              >
+                                <a
+                                  href="whatsapp://send?phone=5543991831471&text=Olá, Vim pelo Site e Gostaria de Saber mais Sobre Questões Admnistrativas!"
+                                  >Enviar Mensagem</a
+                                >
+                              </button>
+                            </div>
+
+                            <div
+                              class="flex flex-col sm:flex-row gap-6 mt-8 inset-0 flex items-center"
+                            >
+                              <p class="text-sm text-gray-500">
+                                Fretes - (43) 99614-0507
+                              </p>
+                              <button
+                                type="button"
+                                class="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
+                              >
+                                <a
+                                  href="whatsapp://send?phone=5543996140507&text=Olá, Vim pelo Site e Gostaria de Saber mais Sobre Questões de Frete!"
+                                  >Enviar Mensagem</a
+                                >
+                              </button>
+                            </div>
+
+                            <div
+                              class="flex flex-col sm:flex-row gap-6 mt-8 inset-0 flex items-center"
+                            >
+                              <p class="text-sm text-gray-500">
+                                Entregas - (43) 99867-5702
+                              </p>
+                              <button
+                                type="button"
+                                class="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
+                              >
+                                <a
+                                  href="whatsapp://send?phone=5543998675702&text=Olá, Vim pelo Site e Gostaria de Saber mais Sobre Questões de Entrega!"
+                                  >Enviar Mensagem</a
+                                >
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="mt-4"></div>
+                      </DialogPanel>
+                    </TransitionChild>
+                  </div>
+                </div>
+              </Dialog>
+            </TransitionRoot>
+            <!-- Fim - Button Modal #1-->
       </ul>
       <div class="flex sm:hidden flex-1 justify-end"><i class="text-xl fas fa-bars text-white cursor-pointer"></i></div>
   </nav>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/vue";
 
+const isOpen = ref(false);
 
+function closeModal() {
+  isOpen.value = false;
+}
+function openModal() {
+  isOpen.value = true;
+}
 </script>
 
